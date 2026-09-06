0a67f406aff601daff4dbfb92aef787858f13361
mohamadarifin97 <itachisooltan@gmail.com>
2026-09-07 02:00:21 +0800

    force_true on terraform destory

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = "devops-bootcamp/final-project-${local.nama}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
